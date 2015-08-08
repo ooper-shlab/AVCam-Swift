a2983c4f45de478c8762d53f1012338377d6141b
OOPer <nagata@shlab.jp>
2015-08-08 10:39:06 +0900

    Adapted to Xcode 7 beta 5

--- AVCam/AAPLCameraViewController.swift
+++ AVCam/AAPLCameraViewController.swift
@@ -446,8 +446,8 @@ class AAPLCameraViewController: UIViewController, AVCaptureFileOutputRecordingDe
                 AAPLCameraViewController.setFlashMode(AVCaptureFlashMode.Off, forDevice: self.videoDeviceInput.device)
                 
                 // Start recording to a temporary file.
-                let outputFileName = NSProcessInfo.processInfo().globallyUniqueString
-                let outputFilePath = NSTemporaryDirectory().stringByAppendingPathComponent(outputFileName.stringByAppendingPathExtension("mov")!)
+                let outputFileName = NSProcessInfo.processInfo().globallyUniqueString as NSString
+                let outputFilePath = (NSTemporaryDirectory() as NSString).stringByAppendingPathComponent(outputFileName.stringByAppendingPathExtension("mov")!)
                 self.movieFileOutput.startRecordingToOutputFileURL(NSURL(fileURLWithPath: outputFilePath), recordingDelegate: self)
             } else {
                 self.movieFileOutput.stopRecording()
@@ -543,8 +543,8 @@ class AAPLCameraViewController: UIViewController, AVCaptureFileOutputRecordingDe
                                         }
                                 })
                             } else {
-                                let temporaryFileName = NSProcessInfo().globallyUniqueString
-                                let temporaryFilePath = NSTemporaryDirectory().stringByAppendingPathComponent(temporaryFileName.stringByAppendingPathExtension("jpg")!)
+                                let temporaryFileName = NSProcessInfo().globallyUniqueString as NSString
+                                let temporaryFilePath = (NSTemporaryDirectory() as NSString).stringByAppendingPathComponent(temporaryFileName.stringByAppendingPathExtension("jpg")!)
                                 let temporaryFileURL = NSURL(fileURLWithPath: temporaryFilePath)
                                 
                                 PHPhotoLibrary.sharedPhotoLibrary().performChanges({
